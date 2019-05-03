4f150e390bfdd70e5389c51b1c6ce92ce2e2c98c
Jérôme Pogeant <p-jerome@hotmail.fr>
2019-05-03 17:36:58 +0200

    :sparkles: Create function logic

--- henri-facts/handler.go
+++ henri-facts/handler.go
@@ -1,10 +1,40 @@
 package function
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"time"
 )
 
-// Handle a serverless request
+type facts struct {
+	Facts []string `json:"facts"`
+}
+
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	jsonFile, err := os.Open("facts.json")
+
+	if err != nil {
+		fmt.Println("Failed to open JSON file:", err)
+	}
+
+	defer jsonFile.Close()
+
+	byteArray, _ := ioutil.ReadAll(jsonFile)
+
+	var facts facts
+
+	if err := json.Unmarshal(byteArray, &facts); err != nil {
+		fmt.Println("Failed to unmarshal JSON:", err)
+	}
+
+	return facts.Facts[randomInt(0, len(facts.Facts))]
+}
+
+func randomInt(min int, max int) int {
+	return min + rand.Intn(max-min)
 }
